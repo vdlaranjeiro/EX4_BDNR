1652728fc98fab17b37d384b574f297237026f08
Vinicius <yinioliveira@gmail.com>
2023-11-16 17:49:57 -0300

    adicionei os arquivos do exercício

---
+++ src/menu.py
@@ -0,0 +1,86 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+import json
+
+from usuario import *
+from vendedor import *
+from produto import *
+
+
+cloud_config= {
+  'secure_connect_bundle': 'secure-connect-bdnr.zip'
+}
+
+with open("yinioliveira@gmail.com-token.json") as f:
+    secrets = json.load(f)
+
+CLIENT_ID = secrets["clientId"]
+CLIENT_SECRET = secrets["secret"]
+
+auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect()
+
+opcao = 0
+while(opcao != 'S'):
+    print("\n1 - CRUD de Usuários")
+    print("2 - CRUD de Vendedores")
+    print("3 - CRUD de Produtos")
+    opcao = input("Selecione uma opção. (S para sair) ").upper()
+
+    acao = 0
+    match opcao:
+        case '1':
+            while(acao != 'V'):
+                print("\n --- CRUD de Usuários ---")
+                print("1 - Cadastrar um novo usuário")
+                print("2 - Excluir um usuário")
+                print("3 - Listar informações de um usuário")
+                print("4 - Atualizar informações de um usuário")
+                print("5 - Realizar uma compra")
+                acao = input("Selecione uma ação. (V para voltar) ").upper()
+                match acao:
+                    case '1':
+                        create_usuario(session)
+                    case '2':
+                        delete_usuario(session)
+                    case '3':
+                        read_usuario(session)
+                    case '4':
+                        update_usuario(session)
+                    case '5':
+                        compra_usuario(session)
+        case '2':
+             while(acao != 'V'):
+                print("\n --- CRUD de Vendedores ---")
+                print("1 - Cadastrar um novo vendedor")
+                print("2 - Excluir um vendedor")
+                print("3 - Listar informações de um vendedor")
+                print("4 - Atualizar informações de um vendedor")
+                acao = input("Selecione uma ação. (V para voltar) ").upper()
+                match acao:
+                    case '1':
+                        create_vendedor(session)
+                    case '2':
+                        delete_vendedor(session)
+                    case '3':
+                        read_vendedor(session)
+                    case '4':
+                        update_vendedor(session)
+        case '3':
+             while(acao != 'V'):
+                print("\n --- CRUD de Produtos ---")
+                print("1 - Cadastrar um novo produto")
+                print("2 - Excluir um produto")
+                print("3 - Listar informações de um produto")
+                print("4 - Atualizar informações de um produto")
+                acao = input("Selecione uma ação. (V para voltar) ").upper()
+                match acao:
+                    case '1':
+                        create_produto(session)
+                    case '2':
+                        delete_produto(session)
+                    case '3':
+                        read_produto(session)
+                    case '4':
+                        update_produto(session)
